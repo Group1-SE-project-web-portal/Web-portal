71d2ea8234288eeefe047023dced0232c4f5621b
Hopson Gausi <jspyhack@gmail.com>
2019-12-26 11:06:52 +0200

    changed charts styling to grid(side by side) using via material ui grid

--- src/App.js
+++ src/App.js
@@ -1,86 +1,108 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom'
-import Dash1 from './pages/Dash1'
-import Dashboards from './pages/Dashboards'
-import NavBar from './components/NavBar.js'
-import Charts from './components/Chart'
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Dash1 from "./pages/Dash1";
+import Dashboards from "./pages/Dashboards";
+import NavBar from "./components/NavBar.js";
+import Charts from "./components/Chart";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
 
+import {
+  getAllDashboards,
+  getCharts,
+  oneChart,
+  getData,
+  request
+} from "./utils/APIUtils";
+import { BASE_URL } from "./constants/Constants";
 
-import { getAllDashboards, getCharts, oneChart, getData, request } from './utils/APIUtils'
-import { BASE_URL } from './constants/Constants';
+//Material ui grid styling
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    paddingTop: theme.spacing.unit * 5
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.primary
+  }
+});
 
-
-
-
+// App component
 class App extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      charts: '',
-      dashId: '',
+      charts: "",
+      dashId: "",
       isLoading: false,
-      singleDashboard: '',
-      dashboards: '',
-      dataElements: '',
-      organisationUnits: ''
-
-    }
+      singleDashboard: "",
+      dashboards: "",
+      dataElements: "",
+      organisationUnits: ""
+    };
   }
   componentDidMount() {
-
-    getCharts().then(data => { this.setState({ charts: data.charts }) })
-    getAllDashboards().then(data => { this.setState({ dashboards: data.dashboards }) })
-    oneChart().then(data => { this.setState({ dashId: data }) })
-
-
+    getCharts().then(data => {
+      this.setState({ charts: data.charts });
+    });
+    getAllDashboards().then(data => {
+      this.setState({ dashboards: data.dashboards });
+    });
+    oneChart().then(data => {
+      this.setState({ dashId: data });
+    });
   }
 
-
   render() {
+    const { classes } = this.props; //material ui class definition
 
-
-    const { charts, dashboards, dashId } = this.state
+    const { charts, dashboards, dashId } = this.state;
 
     if (!charts || !dashboards) {
-      return <div>Loading....</div>
+      return <div>Loading....</div>;
     }
 
-    const dashboardOneItems = dashboards[0].dashboardItems
-    const itemsIds = []
+    const dashboardOneItems = dashboards[0].dashboardItems;
+    const itemsIds = [];
 
     for (let i = 0; i < dashboardOneItems.length; i++) {
       if (dashboardOneItems[i].chart) {
-        itemsIds.push(dashboardOneItems[i].chart)
+        itemsIds.push(dashboardOneItems[i].chart);
       }
     }
 
-    let requiredCharts = []
+    let requiredCharts = [];
 
     charts.forEach(keys1 => {
-
       if (itemsIds.find(keys2 => keys2.id === keys1.id)) {
-        requiredCharts.push(keys1)
+        requiredCharts.push(keys1);
       }
-    })
+    });
 
     //prepare chart One
     const chartOneMetadata = requiredCharts[6];
 
-    const chartOneName = chartOneMetadata.displayName
-    const chartOneType = chartOneMetadata.type.toLowerCase()
-    const orgFilter = chartOneMetadata.series
-    const periodFilter = chartOneMetadata.category
-    const periods = 'LAST_12_MONTHS'
-    const dataDimension = chartOneMetadata.dataDimensionItems[0].dataElement.id
-    const orgUnits = chartOneMetadata.organisationUnits.map(ids => ids.id)
+    const chartOneName = chartOneMetadata.displayName;
+    const chartOneType = chartOneMetadata.type.toLowerCase();
+    const orgFilter = chartOneMetadata.series;
+    const periodFilter = chartOneMetadata.category;
+    const periods = "LAST_12_MONTHS";
+    const dataDimension = chartOneMetadata.dataDimensionItems[0].dataElement.id;
+    const orgUnits = chartOneMetadata.organisationUnits.map(ids => ids.id);
 
     const dataValues = request({
-      url: BASE_URL + `/analytics.json?dimension=dx:${dataDimension};&dimension=ou:${orgUnits[0]};${orgUnits[1]};${orgUnits[2]};${orgUnits[3]};${orgUnits[4]}&dimension=pe:${periods}`,
-                        method: "GET"
-    }).then(data => { console.log(data) })
+      url:
+        BASE_URL +
+        `/analytics.json?dimension=dx:${dataDimension};&dimension=ou:${orgUnits[0]};${orgUnits[1]};${orgUnits[2]};${orgUnits[3]};${orgUnits[4]}&dimension=pe:${periods}`,
+      method: "GET"
+    }).then(data => {
+      console.log(data);
+    });
 
     // const dimensionOne = chartOneMetadata.filterDimension;
 
@@ -95,9 +117,8 @@ class App extends Component {
     // // const value2 = rows[0][2]
     // const value3 = rows[0][2]
 
-
     // requiredCharts[0].
-    console.log(chartOneMetadata)
+    console.log(chartOneMetadata);
     const dataPoints = [
       { label: "JAN", y: 104 },
       { label: "FEB", y: 27 },
@@ -111,36 +132,57 @@ class App extends Component {
       { label: "OCT", y: 420 },
       { label: "NOV", y: 37 },
       { label: "DEC", y: 900 }
-    ]
-
+    ];
 
     const chartOne = () => {
-      return <Charts type={chartOneType} title={chartOneName} data={dataPoints} />
-    }
+      return (
+        <Charts type={chartOneType} title={chartOneName} data={dataPoints} />
+      );
+    };
     const chartTwo = () => {
-      return <Charts type={"bar"} title={"hiv fact"} data={dataPoints} />
-    }
+      return <Charts type={"bar"} title={"hiv fact"} data={dataPoints} />;
+    };
     return (
-      <div className="App" >
+      <div className="App">
+        <NavBar
+          dash1={dashboards[3].displayName}
+          dash2={dashboards[6].displayName}
+          dash3={dashboards[0].displayName}
+          dash4={dashboards[1].displayName}
+        />
 
-        <NavBar dash1={dashboards[3].displayName} dash2={dashboards[6].displayName} dash3={dashboards[0].displayName} dash4={dashboards[1].displayName} />
         <Switch>
-          <Route exact path="/" component={() =>
-            <Dash1 >
-              {chartOne()}
-              {chartTwo()}
-            </Dash1>} />
+          <div className={classes.root}>
+            <Grid container spacing={3}>
+              <Grid item xs>
+                <Paper className={classes.paper}>
+                  {" "}
+                  <Route
+                    exact
+                    path="/"
+                    component={() => <Dash1>{chartOne()}</Dash1>}
+                  />
+                </Paper>
+              </Grid>
+              <Grid item xs>
+                <Paper className={classes.paper}>
+                  {" "}
+                  <Route
+                    exact
+                    path="/"
+                    component={() => <Dash1>{chartTwo()}</Dash1>}
+                  />
+                </Paper>
+              </Grid>
+            </Grid>
+          </div>
           <Route exact path="/dash2" component={Dashboards} />
           <Route exact path="/dash3" component={Dashboards} />
           <Route exact path="/dash4" component={Dashboards} />
-
         </Switch>
-
       </div>
-
-    )
-
+    );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/pages/Dash1.js
+++ src/pages/Dash1.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Dash1 extends Component {
-
-    render(props) {
-
-        return (
-            <div>
-                <div>{this.props.children}</div>
-
-            </div>
-        )
-    }
-
+  render(props) {
+    return (
+      <div>
+        <div>{this.props.children}</div>
+      </div>
+    );
+  }
 }
 
-export default Dash1
+export default Dash1;
